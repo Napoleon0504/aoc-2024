de1adef9095bd39e063c365d06088ccdee8649e0
Oskar Olofsson <oskarjolofsson@gmail.com>
2025-01-08 11:31:16 +0100

    day 4 setup

---
+++ day_4/find_xmas.py
@@ -0,0 +1,10 @@
+# Find xmas:
+# horizontal forwards
+# horizontal backwards
+# vertical up
+# vertical down
+# diagonal right down
+# diagonal left down
+# diagonal right up
+# diagonal left up
+# overlapping other words?
