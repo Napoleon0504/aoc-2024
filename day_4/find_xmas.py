d0c336d440490a7cf27658605f4e8e2f11fde826
Oskar Olofsson <oskarjolofsson@gmail.com>
2025-01-08 17:00:57 +0100

    day 4 horizontal pattern vertical pattern

--- day_4/find_xmas.py
+++ day_4/find_xmas.py
@@ -8,3 +8,36 @@
 # diagonal right up
 # diagonal left up
 # overlapping other words?
+
+import textwrap
+import re
+
+def main():
+    pass
+
+# Find patterns horizontally 
+def find_horizontal_pattern():
+    length = 0
+    with open("xmas.txt", 'r') as f:
+        for line in f.readlines():
+            # Find the pattern xmas or samx
+            matches1 = re.findall(r'XMAS|SAMX',line)
+            length += len(matches1)
+    f.close()
+    return length
+
+def find_vertical_pattern():
+    length = 0
+    with open("xmas.txt", 'r') as f:
+        text = f.read()
+
+    list_of_strings = [''.join(chars) for chars in zip(*text.splitlines())]
+
+    for line in list_of_strings:
+        matches1 = re.findall(r'XMAS|SAMX',line)
+        length += len(matches1)
+    return length
+
+
+print(find_vertical_pattern())
+print(find_horizontal_pattern())
