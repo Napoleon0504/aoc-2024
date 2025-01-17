4ac9529d2c324bb3fd851df9ea32ab5146539c29
Oskar Olofsson <oskarjolofsson@gmail.com>
2025-01-17 13:45:20 +0100

    day 5 part 2

--- day_5/main.py
+++ day_5/main.py
@@ -6,11 +6,18 @@ def main():
     with open(updates_file, 'r') as f:
         txt = f.read()
     updates = txt.split("\n")
-    sum = 0
+    sum_correct = 0
+    sum_wrong = 0
     for update in updates:
         correct_order1 = Order(update, rules_file)
-        if correct_order1.correct_order():
-            sum += correct_order1.correct_order()
-    print(sum)
+        if correct_order1.find_correct_order():
+            sum_correct += correct_order1.find_correct_order()
+            #print("Correct Order: ", update)
+        if  correct_order1.correct_wrong_order():
+            sum_wrong += correct_order1.correct_wrong_order()
+            #print("Wrong order: ", update)
+    print("Sum_correct: ", sum_correct)
+    print("Sum_wrong: ",sum_wrong)
+
 
 main()
--- day_5/order.py
+++ day_5/order.py
@@ -3,8 +3,8 @@ class Order:
     def __init__(self, update, rules):
         self.update = update
         self.rules = rules
-
-    def correct_order(self):
+    
+    def rules_list(self):
         # This is an order-method that takes in a rules-file and spits put an correct order
         # Get list with all rules
         with open(self.rules, 'r') as f:
@@ -14,10 +14,16 @@ class Order:
         # Split ever element into lists
         for i in range(len(rules)):
             rules[i] = rules[i].split("|")
+        return rules
+    
+    def update_list(self):
         # Put update in list
         update = self.update.split(",")
-        # Sort all nums
-        
+        return update
+
+    def find_correct_order(self):
+        update = self.update_list()
+        rules = self.rules_list()
         for i in range(len(update)):
             for j in range(len(rules)):
                 # Check if the number we are checking is in the rules[j]
@@ -39,9 +45,31 @@ class Order:
         middle_term = update[len(update) // 2]
         return int(middle_term)
         
-    
-        
+    def correct_wrong_order(self):
+        update = self.update_list()
+        rules = self.rules_list()
+        any_swap = False    # Track if any swap was made during the 
 
-                            
-                        
-        
+        # Simple bubble sort
+        n = len(update)
+        for i in range(n):
+            for j in range(0, n - i - 1):
+                current = update[j]
+                next_num = update[j + 1]
+
+                # Check if this pair needs to be swaped
+                for rule in rules:
+                    if current in rule and update[j + 1] in rule:
+                        # Check if they are in the right position
+                        if rule[0] != update[j] and rule[1] != update[j + 1]:
+                            update[j], update[j + 1] = update[j + 1], update[j]
+                            any_swap = True
+                            break
+
+        # Check if update has been tampered with
+        if any_swap:
+            # Check middle element in update and 
+            middle_term = update[len(update) // 2]
+            return int(middle_term)
+        return False
+
